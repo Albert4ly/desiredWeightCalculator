08e40d851c5dae76ac304a5284e049eaaaedcf59
Albert <a91614830@gmail.com>
2024-04-27 22:30:12 +0200

    feature:inputs-range-validator

--- js/desiredWeightCalculator.js
+++ js/desiredWeightCalculator.js
@@ -4,13 +4,19 @@ const initialWeightSpan = document.querySelector(".appCnt__initialWeightSpan");
 const desiredWeightSpan = document.querySelector(".appCnt__desiredWeightSpan");
 const heightSpan = document.querySelector(".appCnt__heightSpan");
 
+const submitBtn = document.querySelector(".appCnt__submitBtn");
+
 export class DesiredWeightCalculator {
    constructor() {
+      this.initialWeight = null;
+      this.desiredWeight = null;
+
       this.listenerFunc();
    }
 
    listenerFunc() {
       form.addEventListener('click', (e) => this.changeRangeInputValue(e))
+      submitBtn.addEventListener('click', (e) => this.dataValidation(e));
    }
    
    changeRangeInputValue(e) {
@@ -27,6 +33,33 @@ export class DesiredWeightCalculator {
       if (e.target.classList.contains("appCnt__inputHeight")) {
          heightSpan.innerText = e.target.value;
       }
+   }
+
+   dataValidation(e) {
+      e.preventDefault();
+      const errorAlert = document.querySelector(".appCnt__errorAlertSection");
+      
+      this.initialWeight = initialWeightSpan.innerText;
+      this.desiredWeight = desiredWeightSpan.innerText;
+
+      if (this.initialWeight === this.desiredWeight) {
+
+         if (errorAlert.classList.contains("appCnt__errorAlertSection")) {
+            errorAlert.classList.remove("appCnt__errorAlertSection");
+            errorAlert.classList.add("appCnt__errorAlertSectionDisplay");
+         }
+      }
+
+      if (this.initialWeight !== this.desiredWeight) {
+
+         const errorAlert = document.querySelector(".appCnt__errorAlertSectionDisplay");
+
+         if (errorAlert.classList.contains("appCnt__errorAlertSectionDisplay")) {
+            errorAlert.classList.remove("appCnt__errorAlertSectionDisplay");
+            errorAlert.classList.add("appCnt__errorAlertSection");
+         }
+      }
+     
    }
 
 }
